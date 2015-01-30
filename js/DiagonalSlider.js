107b46a45bab2eac548c78e47894658dd9134818
Alexis Espósito <aesposito@innvenio.com>
2015-01-30 14:42:32 -0200

    Init

---
+++ js/DiagonalSlider.js
@@ -0,0 +1,100 @@
+/*
+validate_form
+jQuery plugin to create diagonal slider
+(c) Innvenio 2015 (@innvenio)
+*/
+
+function loadSlider(slider,default_text){
+    var w;
+    var width = 0;
+    var image_width = slider.find('.gallery_item img').width();
+    var image_height = slider.find('.gallery_item img').height();
+    var out = true;
+    var valor = 0;        
+    var length_gallery_item = slider.find('.gallery_item').length;
+
+    valor = length_gallery_item * 25;
+    if($(window).width()<1060){
+        valor = length_gallery_item * 40;
+    }
+    w = $(window).width() + ($(window).width() / length_gallery_item) + valor;
+    width = w / 3;
+    slider.width(w);
+    slider.height($(window).height());
+    slider.find('.gallery_item').width((w / length_gallery_item));
+    slider.find('.gallery_item img').css('margin-left', ((image_width / 2) * -1) + (w / length_gallery_item));
+    if($(window).height()<image_height){            
+        slider.find('.gallery_item img').css('top', ((image_height-$(window).height())/2)*-1);
+    }
+    var i = 1;
+    slider.find('.gallery_item').each(function(){
+        $(this).attr('data-position', i);
+        i++;
+    });
+
+    height_img = slider.find('.gallery_item img').height();
+
+    if(height_img<slider.height()){
+        slider.height(height_img);
+    }
+
+    $('.content_slider').find('.content_title').css('margin-top',slider.height() - 200); 
+
+    slider.find('.gallery_item').unbind("hover");
+    slider.find('.gallery_item').hover(function(){
+        if (out){
+            zoomIn($(this), function(){
+                out = false;    
+            }); 
+        }
+
+    }, function(){
+        zoomOut(function(){
+            out = true; 
+        });
+    });
+
+    function zoomOut(callback){
+        slider.find('.gallery_item').each(function(){
+            var x = w / length_gallery_item;
+            $(this).css('width', x);
+        });
+        $('.content_slider').find('.content_title .text').html(default_text);
+        callback();
+    }
+
+    function zoomIn(item, callback){
+        slider.find('.gallery_item').each(function(){
+            var x = (w / length_gallery_item) - (width / length_gallery_item-1);
+            if ($(this).attr('data-position') != item.attr('data-position')){
+                $(this).css('width', x);
+            }
+            else
+            {
+                item.css('width', ((w / length_gallery_item) + width) - ((width / length_gallery_item) * 1.5));
+                var title = item.find('img').attr('data-title');
+                $('.content_slider').find('.content_title .text').html(title);
+            }
+        });
+        callback();
+    }
+}
+
+(function($) {
+    $.fn.createDiagonalSlider = function(default_text) {
+        var slider = $(this);
+        var doit;
+
+        loadSlider(slider, default_text);        
+        function resizedw(){
+            loadSlider(slider, default_text);
+        }
+        window.onresize = function() {
+            clearTimeout(doit);
+            doit = setTimeout(function() {
+                resizedw();
+            }, 100);
+        };
+
+    }
+}(jQuery));
---
+++ js/script.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+	$('.gallery_content').createDiagonalSlider("<b>Uruguay apuesta al crecimiento de la vida</b><br/>a través de sus políticas de desarrollo productivo y humano");
+});

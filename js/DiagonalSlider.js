332c5fe958e69d83145a4b14978aeb640d8cf470
Alexis Espósito <aesposito@innvenio.com>
2015-05-08 22:46:21 -0300

    Bug firefox

--- js/DiagonalSlider.js
+++ js/DiagonalSlider.js
@@ -10,11 +10,12 @@ function loadSlider(slider,default_text){
     var image_width = slider.find('.gallery_item img').width();
     var image_height = slider.find('.gallery_item img').height();
     var out = true;
-    var valor = 0;        
+    var valor = 0;    
+    var timeout;
     var length_gallery_item = slider.find('.gallery_item').length;
 
     valor = length_gallery_item * 25;
-    if($(window).width()<1060){
+    if($(window).width() < 1060){
         valor = length_gallery_item * 40;
     }
     w = $(window).width() + ($(window).width() / length_gallery_item) + valor;
@@ -34,7 +35,7 @@ function loadSlider(slider,default_text){
 
     height_img = slider.find('.gallery_item img').height();
 
-    if(height_img<slider.height()){
+    if(height_img < slider.height()){
         slider.height(height_img);
     }
 
@@ -42,10 +43,16 @@ function loadSlider(slider,default_text){
 
     slider.find('.gallery_item').unbind("hover");
     slider.find('.gallery_item').hover(function(){
+        var item = $(this);
         if (out){
-            zoomIn($(this), function(){
-                out = false;    
-            }); 
+            out = false;
+            if (timeout){
+                clearTimeout(timeout);    
+            }
+
+            timeout = setTimeout(function(){
+                zoomIn(item, function(){ }); 
+            }, 10);
         }
 
     }, function(){
@@ -59,13 +66,19 @@ function loadSlider(slider,default_text){
             var x = w / length_gallery_item;
             $(this).css('width', x);
         });
-        $('.content_slider').find('.content_title .text').html(default_text);
+
+        setTimeout(function(){
+            if (out) {
+                $('.content_slider').find('.content_title .text').html(default_text);    
+            }
+        }, 200);
+        
         callback();
     }
 
     function zoomIn(item, callback){
         slider.find('.gallery_item').each(function(){
-            var x = (w / length_gallery_item) - (width / length_gallery_item-1);
+            var x = (w / length_gallery_item) - (width / length_gallery_item - 1);
             if ($(this).attr('data-position') != item.attr('data-position')){
                 $(this).css('width', x);
             }
@@ -93,6 +106,7 @@ function loadSlider(slider,default_text){
         function resizedw(){
             loadSlider(slider, default_text);
         }
+
         window.onresize = function() {
             clearTimeout(doit);
             doit = setTimeout(function() {
